bd9618d42364577f440fe758fc3efcb95366a2b7
silvio-fullstack <sbalves@bol.com.br>
2020-05-07 21:26:46 -0300

    primeiro commit

---
+++ about/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class About1Config(AppConfig):
+    name = 'about1'
---
+++ tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('hello/', views.helloword),
+    path('', views.tasklist, name='task-list'),
+    path('tasks/<int:id>', views.taskView, name="task-view"),
+    path('newTask/', views.newTask, name="new-task"),
+    path('edit/<int:id>', views.editTask, name="edit-task"),
+]
